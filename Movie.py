89495e04a7bc6bca8c39a6323abe2881e3b70a7b
Satvik Pruthi <satvikpruthi.bt23cseds@pec.edu.in>
2025-07-31 23:42:14 +0530

    Update Movie.py

--- Movie.py
+++ Movie.py
@@ -60,6 +60,17 @@ st.markdown("""
 
 
 
+st.markdown("""
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    .viewerBadge_container__1QSob {display: none;}
+    </style>
+""", unsafe_allow_html=True)
+
+
+
 
 
 
